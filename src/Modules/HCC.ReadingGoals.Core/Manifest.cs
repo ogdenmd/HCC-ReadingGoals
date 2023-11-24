bec90a55d7cd09c2ab740278b4d7920274cabfdf
michael.dunn <michael.dunn@healthcatalyst.com>
2023-11-24 13:43:13 -0800

    initialized project

---
+++ src/Modules/HCC.ReadingGoals.Core/Manifest.cs
@@ -0,0 +1,10 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "HCC.ReadingGoals.Core",
+    Author = "The Orchard Core Team",
+    Website = "https://orchardcore.net",
+    Version = "0.0.1",
+    Description = "HCC.ReadingGoals.Core",
+    Category = "Content Management"
+)]
